d1f50fc69128abefd73bdaefebc838f423d32f51
its your bedtime <you@example.com>
2019-05-14 20:45:43 +0700

    V, h

--- lib/library/h.lua
+++ lib/library/h.lua
@@ -2,7 +2,6 @@ H = function(self, x, y, frame, grid)
   self.name = 'H'
   self.y = y
   self.x = x
-  local ports = {{0, 1 , 'output'}}
   local a = grid[y - 1][x]
   local existing = grid[y + 1][x] == self.list[grid[y + 1][x]] and grid[y + 1][x] or 'null'
   if self:active() then
