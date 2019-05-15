801d78cab6078ef4e46e9ed165583a7ac98185ca
its your bedtime <you@example.com>
2019-05-15 21:41:29 +0700

    comment/

--- lib/library/_comment.lua
+++ lib/library/_comment.lua
@@ -2,21 +2,20 @@ comment = function ( self, x, y, frame, grid )
   self.name = '#'
   self.x = x
   self.y = y
-  
-  for x = self.x + 1, self.XSIZE do
-    grid.params[self.y][x].dot = false
-    grid.params[self.y][x].act = true
-    grid.params[self.y][x].op = true
-    
-    if grid[self.y][x] == self.name then
-      local l = x
-      for c = self.x + 1, l do
-        grid.params[self.y][c].dot = true
-        grid.params[self.y][c].act = false
-        grid.params[self.y][c].op = false
-        grid.params[self.y][c].lit = false
+  --local l = nil
+  for x = x + 1, self.XSIZE do
+    grid.params[y][x].dot = true
+    grid.params[y][x].act = false
+    grid.params[y][x].op = false
+    grid.params[y][x].lit = false
+    if grid[y][x] == self.name then
+      for c = x + 1, self.XSIZE do
+        grid.params[y][c].dot = false
+        grid.params[y][c].act = true
+        grid.params[y][c].op = true
       end
       break
+    else
     end
   end
 end
