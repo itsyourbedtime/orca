0456b5494f9f1c6394b160094f82790dcedda3e9
its your bedtime <you@example.com>
2019-05-17 14:24:00 +0700

    param op tweaks, details in library post

--- lib/library/_timber_param.lua
+++ lib/library/_timber_param.lua
@@ -1,4 +1,5 @@
-local param_ids = { "quality", "amp_env_attack", "amp_env_decay","original_freq", "detune_cents", "by_percentage","filter_type","filter_freq", "filter_resonance"}
+local param_ids = { "amp_env_attack", "amp_env_decay", "original_freq", "detune_cents", 
+"by_percentage","filter_freq", "filter_resonance", "filter_type", "quality" }
 
 timber_param = function ( self, x, y, frame, grid )
   self.name = '"'
@@ -9,11 +10,13 @@ timber_param = function ( self, x, y, frame, grid )
   local param = util.clamp( self:listen( self.x + 2, self.y ) or 1, 1, #param_ids)
   local val = self:listen( self.x + 3, self.y ) or 0
   local val_scaled = math.floor(( val / #self.chars ) * 100 )
-  local value = ( param == 1 or param == 7) and val 
-  or param == 9 and ( val / #self.chars ) 
-  or param == 8 and val_scaled * 200 
-  or (param == 2 or param == 3) and ( val / #self.chars ) * 5
-  or val_scaled
+  local value = (param == 1 or param == 2) and ( val / #self.chars ) * 5 -- attack / decay
+              or param == 5 and val_scaled * 2 -- stretch
+              or param == 6 and val_scaled * 200  -- filter freq
+              or param == 7 and ( val / #self.chars )  -- res
+              or param == 8 and (val % 2) + 1
+              or param == 9 and (val % 4) + 1  
+              or val_scaled
   if self.banged( self.x, self.y ) then
     grid.params[self.y][self.x].lit_out = false
     params:set( param_ids[param] .. "_" .. sample, value )
