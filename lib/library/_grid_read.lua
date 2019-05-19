56bcdca21c9e7eed006f71c55185658be3356bfb
its your bedtime <you@example.com>
2019-05-19 18:07:27 +0700

    fix read

--- lib/library/_grid_read.lua
+++ lib/library/_grid_read.lua
@@ -5,8 +5,9 @@ grid_read = function ( self, x, y, frame, _grid )
   self:spawn(self.ports[self.name])
   local col = util.clamp(self:listen( self.x - 2, self.y ) or 1 % g.cols, 1, g.cols)
   local row = util.clamp(self:listen( self.x - 1, self.y ) or 1 % g.rows, 1, g.rows)
+  local value = (_grid.grid[row][col] ~= nil and _grid.grid[row][col] < 6 and 'null') or _grid.grid[row][col] == nil and 'null' or '*'
   if self:active() then
-    _grid[self.y + 1][self.x] = _grid.grid[row][col] < 6 and 'null' or '*'
+    _grid[self.y + 1][self.x] = value
   end
 end
 
