198abbb9e8d59768d138d6d1b988ba50998d738b
its your bedtime <you@example.com>
2019-05-30 06:00:47 +0700

    G misxy

--- lib/library/g.lua
+++ lib/library/g.lua
@@ -16,19 +16,19 @@ local G = function(self, x, y, glyph)
   local b = util.clamp(self:listen(self.x - 3, self.y) or 1, 1, 35)
   local length = self:listen(self.x - 1, self.y, 0) or 0
   local op = self:listen(self.x + 1, self.y, 0)
-  local offset_x, offset_y = a + self.x - 1, b + self.y 
+  local offset_x, offset_y = b + self.x - 1, a + self.y 
 
     
   for i = 1, length do
     self.ports[#self.ports + 1] = { i , 0 , 'g-value ' .. i,  'input' }
-    self.ports[#self.ports + 1] = { a + i - 1, b , 'g-value ' .. i, (not op and i == 1 ) and 'output' or 'haste' }
+    self.ports[#self.ports + 1] = { b + i - 1, a , 'g-value ' .. i, (not op and i == 1 ) and 'output' or 'haste' }
   end
     
     
   local function operate()
     for i = 1, length do
       local input = self:glyph_at(self.x + i, self.y)
-      self:write( (a + i) - 1 , b, input)
+      self:write( (b + i) - 1 , a, input)
     end
   end
     
