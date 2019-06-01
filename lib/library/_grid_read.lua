7e5d935d79dd8670d8b98f3f7bc44b2bd03cd70d
its your bedtime <you@example.com>
2019-06-01 18:12:24 +0700

    grid read op sliders fix

--- lib/library/_grid_read.lua
+++ lib/library/_grid_read.lua
@@ -11,20 +11,17 @@ local grid_read = function ( self, x, y )
   
   self:spawn(self.ports)
   
-  local GRID_ROWS = self.g.rows
-  local GRID_COLS = self.g.cols
   local col = self:listen( self.x - 2, self.y )
   local row = self:listen( self.x - 1, self.y ) 
-  local mode = (row ~= false and col == false and 1 ) or (row == false and col ~= false and 2) or 0
+  local mode = (row  and not col and 1 ) or (col and not row and 2) or 0
   local v = self.grid[not row and 1 or row][not col and 1 or col] 
   local value = v and v < 6 and '.' or '*'
-
-
+  
   if mode == 0 then
     self.grid[ row or 1 ][ col or 1 ] = 5
     self:write(0, 1, value)
   else 
-    for i= 1,( mode == 1 and GRID_COLS or GRID_ROWS ) do
+    for i= 1,( mode == 1 and self.g.cols or self.g.rows ) do
       local y, x = mode == 1 and row or i, mode == 1 and i or col
       if self.grid[y][x] ~= nil and self.grid[y][x] > 6 then 
         self:write( self.ports[3][1], self.ports[3][2], self.chars[i])
